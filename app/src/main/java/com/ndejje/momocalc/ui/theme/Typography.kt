03782c2920acf8a9d9e2341b3bfb194730f080d2
Sebbi Salama <sebbisalama123@gmail.com>
2026-03-29 23:02:47 +0300

    Declared a font family

---
+++ app/src/main/java/com/ndejje/momocalc/ui/theme/Typography.kt
@@ -0,0 +1,16 @@
+package com.ndejje.momocalc.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.ndejje.momocalc.R
+
+val PlayfairDisplay = FontFamily(
+    Font(R.font.playfair_display_regular, FontWeight.Normal),
+    Font(R.font.playfair_display_bold, FontWeight.Bold)
+)
+
+val SourceSans3 = FontFamily(
+    Font(R.font.source_sans3_regular, FontWeight.Normal),
+    Font(R.font.source_sans3_semibold, FontWeight.SemiBold)
+)
