d5924442275bd83a2b732f81c7208aec3b66086d
Sebbi Salama <sebbisalama123@gmail.com>
2026-03-29 23:12:47 +0300

    Passed the typography into MaterialTheme

--- app/src/main/java/com/ndejje/momocalc/MainActivity.kt
+++ app/src/main/java/com/ndejje/momocalc/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -28,14 +29,17 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.ndejje.momocalc.ui.theme.MoMoTypography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MaterialTheme {
-                Surface { MoMoCalcScreen() }
+            MaterialTheme(typography = MoMoTypography) {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MoMoCalcScreen()
+                }
             }
         }
     }
