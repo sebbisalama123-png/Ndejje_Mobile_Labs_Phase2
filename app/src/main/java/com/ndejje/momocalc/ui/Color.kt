7502db38e06285e04f579b50013fa8e1658f5da8
Sebbi Salama <sebbisalama123@gmail.com>
2026-04-12 00:02:17 +0300

    Created Color.kt

---
+++ app/src/main/java/com/ndejje/momocalc/ui/Color.kt
@@ -0,0 +1,20 @@
+package com.ndejje.momocalc.ui
+
+import androidx.compose.ui.graphics.Color
+
+// Brand palette
+val NavyBlue      = Color(0xFF003087)
+val NavyBlueDark  = Color(0xFF001540)
+val BrandGold     = Color(0xFFC9A227)
+val BrandGoldDark = Color(0xFF9C7A1A)
+
+// Neutrals
+val White         = Color(0xFFFFFFFF)
+val LightGrey     = Color(0xFFF4F6FB)
+val DarkSurface   = Color(0xFF1A1A2E)
+val DarkBackground= Color(0xFF0D1B3E)
+val OnDarkText    = Color(0xFFE8F0FE)
+
+// Semantic
+val ErrorRed      = Color(0xFFC0392B)
+val OnErrorWhite  = Color(0xFFFFFFFF)
